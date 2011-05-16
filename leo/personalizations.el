7786d6f2bdb989223b0f2a0734d6369a5714ee5d
Leonard Axelsson <leonard.axelsson@agical.com>
2011-05-16 21:12:41 +0200

    Initial commit of my personalizations.

---
+++ leo/personalizations.el
@@ -0,0 +1,38 @@
+(color-theme-tangotango)
+
+
+;;; nice and big font (Thanks codification! :)
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default
+    ((t
+      (:inherit nil
+                :stipple nil
+                :inverse-video nil
+                :box nil
+                :strike-through nil
+                :overline nil
+                :underline nil
+                :slant normal
+                :weight normal
+                :height 160
+                :width normal
+                :foundry "unknown"
+                :family "Anonymous Pro")))))
+
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("gradle" . groovy-mode))
+
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
